5e5d680de0e0b1f8774da4add6773e1edf648e1d
Mike Loseto <losetom1998@gmail.com>
2022-07-09 10:58:30 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function introduction(name) {
+    if (name === "Aki"){
+        return "Hi, my name is Aki."
+    }
+    else if (name === "Samip"){
+        return "Hi, my name is Samip."
+    }
+}
+
+function introductionWithLanguage(name , language) {
+    if ( (name === "Aki") &&  (language === "Ember.js") ){
+        return "Hi, my name is Aki and I am learning to program in Ember.js."
+    }
+    else if ( (name === "Samip") && (language === "React") ){
+        return "Hi, my name is Samip and I am learning to program in React."
+    }
+}
+
+function introductionWithLanguageOptional(name , language='JavaScript'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+
+    // if ( (name === "Gracie") && (language === "JavaScript") ){
+    //     return "Hi, my name is Gracie and I am learning to program in JavaScript."
+    // }
+    // else if ( (name === "Gracie") && (language === "Python"))
+    //     return "Hi, my name is Gracie and I am learning to program in Python."
+}
+
